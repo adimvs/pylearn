f8e68b2c4c1b46a8ffbd512e4af410584434cb48
Adrian Visoiu <adi@localhost.localdomain>
2018-10-14 16:49:55 +0300

    Added app file

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+person = {'first_name': 'Bill',
+     'last_name': 'Gates',
+     'series': 'RX',
+     'number': '234987',
+     'cnp': '1600011223344'}
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/idcards/extract', methods=['GET','POST'])
+def api_all():
+    return jsonify(person)
+
+if __name__ == "__main__":
+    app.run(port=8080)
